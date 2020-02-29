79a798262211b684ce6ae62721d66a6a0435b347
nelson pino <npino_faces@hotmail.com>
2020-02-29 15:04:52 -0400

    CONFIGURACION DEL API

---
+++ Sales/Services/ApiService.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+using Sales.Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sales.Services
+{
+    public class ApiService
+    {
+        public async Task<Response> GetList<T>(string UrlBase, string Prefix, string Controller) 
+        {
+            try
+            {
+                var Client = new HttpClient
+                {
+                    BaseAddress = new Uri(UrlBase)
+                };
+                var url = $"{Prefix}{Controller}";
+                var response = await Client.GetAsync(url);
+                var answer = await response.Content.ReadAsStringAsync();
+                if (!response.IsSuccessStatusCode)
+                {
+                    return new Response
+                    {
+                        IsSuccess = false,
+                        Message = answer
+                    };
+                }
+                var List = JsonConvert.DeserializeObject<List<T>>(answer);
+                return new Response
+                {
+                    IsSuccess = true,
+                    Result = List
+                };
+            }
+            catch (Exception ex)
+            {
+                return new Response
+                {
+                    IsSuccess = false,
+                    Message = ex.Message
+                };  
+            }
+        }
+    }
+}
